8b910f74d77048923aa6c3a2460da6817f40278d
JohnJohn21121 <john.abreu21121@gmail.com>
2023-07-16 14:02:58 -0300

    basic Project Structure

---
+++ CanDoItWeatherReport/src/main/java/com/JohnJohn21121/CanDoItWeatherReport/CanDoItWeatherReportApplication.java
@@ -0,0 +1,13 @@
+package com.JohnJohn21121.CanDoItWeatherReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanDoItWeatherReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanDoItWeatherReportApplication.class, args);
+	}
+
+}
---
+++ CanDoItWeatherReport/src/test/java/com/JohnJohn21121/CanDoItWeatherReport/CanDoItWeatherReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JohnJohn21121.CanDoItWeatherReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanDoItWeatherReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
